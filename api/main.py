fd560ea529893d8ae249f5aa20f96b503b2f63de
root <root@micro-services.ktech.sn>
2025-12-15 00:40:38 +0000

    Cyber-DevOps Lab deployment - 2025-12-15 00:40:38

--- api/main.py
+++ api/main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI, Header, HTTPException
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI(title="Cyber-DevOps API")
+
+# Instrumentation Prometheus
+Instrumentator().instrument(app).expose(app)
 
 @app.get("/")
 def root():
     return {"status": "API sécurisée opérationnelle"}
 
 @app.get("/secure")
-def secure_endpoint(authorization: str = Header(None)):
+def secure(authorization: str = Header(None)):
     if authorization is None:
         raise HTTPException(status_code=401, detail="Token manquant")
-    return {"message": "Accès autorisé avec token"}
+
+    if not authorization.startswith("Bearer "):
+        raise HTTPException(status_code=401, detail="Format du token invalide")
+
+    return {"message": "Accès autorisé (IAM externe Keycloak)"}
